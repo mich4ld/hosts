39a873a466c4e24e580295e2ae84acd888f0c96d
Michał <43048524+mich4ld@users.noreply.github.com>
2021-08-19 00:25:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
-
 cd "$(dirname "$0")"
+
 HOSTS_PATH='/etc/hosts'
+
 ADS_GAMBLING_PORN_HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts'
 ADS_GAMBLING_HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling/hosts'
 ADS_HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts'
@@ -22,25 +22,33 @@ function append_hosts() {
   cat ./hosts >> $HOSTS_PATH
 }
 
-function main() {
+function install() {
+  if [ $1 -eq 1 ]; then
+    block_ads_only
+  elif [ $1 -eq 2 ]; then
+    block_gambling
+  elif [ $1 -eq 3 ]; then
+    block_gambling_porn
+  else
+    echo "Invalid option"
+  fi
+}
+
+function type_answer() {
   echo 'What do you want to block?'
   echo '1) Ads'
   echo '2) Ads and gambling'
   echo '3) Ads, gambling and porn'
   
   read choice
-  
-  if [ $choice -eq 1 ]; then
-    block_ads_only
-  elif [ $choice -eq 2 ]; then
-    block_gambling
-  elif [ $choice -eq 3 ]; then
-    block_gambling_porn
-  else
-    echo "Invalid option"
-  fi
-  
-  append_hosts
+  install $choice
 }
 
-main
+if [ -n "$1" ]; then
+  install $1
+else
+  type_answer
+fi
+
+append_hosts
+echo "New hosts file generated in: $HOSTS_PATH"
