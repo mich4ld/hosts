a04de2f34829438fb46e5fca66bb815622b9b7d4
Michał <43048524+mich4ld@users.noreply.github.com>
2021-08-19 11:03:12 +0200

    add more options

--- install.sh
+++ install.sh
@@ -8,6 +8,11 @@ ADS_HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts'
 ADS_SOCIAL_HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/social/hosts'
 ADS_FAKENEWS_HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews/hosts'
 ADS_GAMBLING_HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling/hosts'
+ADS_PORN_HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts'
+
+ADS_FAKENEWS_GAMBLING_HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts'
+ADS_FAKENEWS_PORN_HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-porn/hosts'
+ADS_FAKENEWS_SOCIAL_HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-social/hosts'
 
 ADS_GAMBLING_PORN_HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts'
 ADS_GAMBLING_PORN_FAKENEWS_HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts'
@@ -25,8 +30,24 @@ function block_social() {
   curl $ADS_SOCIAL_HOSTS > $HOSTS_PATH
 }
 
+function block_porn() {
+  curl $ADS_PORN_HOSTS > $HOSTS_PATH
+}
+
 function block_fakenews() {
   curl $ADS_FAKENEWS_HOSTS > $HOSTS_PATH
+}
+
+function block_fakenews_gambling() {
+  curl $ADS_FAKENEWS_GAMBLING_HOSTS > $HOSTS_PATH
+}
+
+function block_fakenews_porn() {
+  curl $ADS_FAKENEWS_PORN_HOSTS > $HOSTS_PATH
+}
+
+function block_fakenews_social() {
+  curl $ADS_FAKENEWS_SOCIAL_HOSTS > $HOSTS_PATH
 }
 
 function block_gambling_porn() {
@@ -55,12 +76,20 @@ function install() {
   elif [ $1 -eq 3 ]; then
     block_social
   elif [ $1 -eq 4 ]; then
-    block_fakenews
+    block_porn
   elif [ $1 -eq 5 ]; then
-    block_gambling_porn
+    block_fakenews
   elif [ $1 -eq 6 ]; then
-    block_gambling_porn_fakenews
+    block_fakenews_gambling
   elif [ $1 -eq 7 ]; then
+    block_fakenews_porn
+  elif [ $1 -eq 8 ]; then
+    block_fakenews_social
+  elif [ $1 -eq 9 ]; then
+    block_gambling_porn
+  elif [ $1 -eq 10 ]; then
+    block_gambling_porn_fakenews
+  elif [ $1 -eq 11 ]; then
     block_gambling_porn_fakenews_social
   else
     echo "Invalid option"
@@ -73,10 +102,14 @@ function type_answer() {
   echo '1) Ads'
   echo '2) Ads and gambling'
   echo '3) Ads and social'
-  echo '4) Ads and fakenews'
-  echo '5) Ads, gambling and porn'
-  echo '6) Ads, gambling, porn and fakenews'
-  echo '7) Ads, gambling, porn, fakenews and social'
+  echo '4) Ads and porn'
+  echo '5) Ads and fakenews'
+  echo '6) Ads, fakenews and gambling'
+  echo '7) Ads, fakenews and porn'
+  echo '8) Ads, fakenews and social'
+  echo '9) Ads, gambling and porn'
+  echo '10) Ads, gambling, porn and fakenews'
+  echo '11) Ads, gambling, porn, fakenews and social'
   
   read choice
   install $choice
